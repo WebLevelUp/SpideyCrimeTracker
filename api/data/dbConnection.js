f13eecaab2dfd9e306aa07a4acd3995de4b21e81
BBDNET\bbdnet2874 <mcquile@bbd.co.za>
2024-05-02 17:27:17 +0200

    Added dbConnection.js. Contains logic using tedious to connect the RDS DB. Connection secrets handled by .env. Modified index.js to run a test query.

---
+++ api/data/dbConnection.js
@@ -0,0 +1,41 @@
+import 'dotenv/config'
+import {Connection, Request} from 'tedious';
+
+let config = {
+    server: process.env.DB_HOST,
+    options: {
+        encrypt: false,
+        trustServerCertificate: true,
+    },
+    authentication: {
+        type: "default",
+        options: {
+            userName: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+        }
+    }
+};
+
+export let connection = new Connection(config);
+
+export function executeStatement(query, params = []) {
+    let request = new Request(query, function (err, rowCount) {
+        if (err) {
+            console.error('Request error:', err);
+        } else {
+            console.log(`${rowCount} rows`);
+            connection.close();
+        }
+    });
+
+    request.on('row', function (columns) {
+        const rowValues = columns.map((column) => `${column.metadata.colName}: ${column.value}`);
+        console.log('Row:', rowValues.join(', '));
+    });
+
+    for (let param of params) {
+        request.addParameter(param.name, param.type, param.value);
+    }
+
+    connection.execSql(request);
+}
